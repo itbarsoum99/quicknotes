be1840448907ce2bb07d1fc924b1c94b0c3d8152
Barsoum <barsoum@Barsoums-iMac.attlocal.net>
2021-10-31 17:45:44 -0400

    first commit

---
+++ notes/ContentView.swift
@@ -0,0 +1,57 @@
+//
+//  ContentView.swift
+//  notes
+//
+//  Created by Barsoum on 10/31/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var note1 = ""
+    @State private var note2 = ""
+    @State private var note3 = ""
+    @State private var note4 = ""
+    @State private var note5 = ""
+    @State private var note6 = ""
+
+    var body: some View {
+        ScrollView {
+            Text("Quick Notes")
+                .fontWeight(.bold)
+                .padding()
+                .font(.title)
+            TextEditor(text: $note1)
+                .frame(height: 200)
+                .padding()
+                .border(Color(hue: 0.566, saturation: 0.447, brightness: 1.0))
+            TextEditor(text: $note2)
+                .frame(height: 200)
+                .padding()
+                .border(Color(hue: 0.566, saturation: 0.447, brightness: 1.0))
+            TextEditor(text: $note3)
+                .frame(height: 200)
+                .padding()
+                .border(Color(hue: 0.566, saturation: 0.447, brightness: 1.0))
+            TextEditor(text: $note4)
+                .frame(height: 200)
+                .padding()
+                .border(Color(hue: 0.566, saturation: 0.447, brightness: 1.0))
+            TextEditor(text: $note5)
+                .frame(height: 200)
+                .padding()
+                .border(Color(hue: 0.566, saturation: 0.447, brightness: 1.0))
+            TextEditor(text: $note6)
+                .frame(height: 200)
+                .padding()
+                .border(Color(hue: 0.566, saturation: 0.447, brightness: 1.0))
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ notes/notesApp.swift
@@ -0,0 +1,17 @@
+//
+//  notesApp.swift
+//  notes
+//
+//  Created by Barsoum on 10/31/21.
+//
+
+import SwiftUI
+
+@main
+struct notesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
